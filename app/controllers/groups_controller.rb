4e18188c93db6e382597a65ab7a214a080e22602
djkazunoko <djkazunoko@gmail.com>
2024-03-19 17:24:25 +0900

    style: 不要なコメントを削除 (#57)

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,25 +1,20 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: %i[ show edit update destroy ]
 
-  # GET /groups or /groups.json
   def index
     @groups = Group.all
   end
 
-  # GET /groups/1 or /groups/1.json
   def show
   end
 
-  # GET /groups/new
   def new
     @group = Group.new
   end
 
-  # GET /groups/1/edit
   def edit
   end
 
-  # POST /groups or /groups.json
   def create
     @group = Group.new(group_params)
 
@@ -30,7 +25,6 @@ class GroupsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /groups/1 or /groups/1.json
   def update
     if @group.update(group_params)
       redirect_to group_url(@group), notice: "Group was successfully updated."
@@ -39,7 +33,6 @@ class GroupsController < ApplicationController
     end
   end
 
-  # DELETE /groups/1 or /groups/1.json
   def destroy
     @group.destroy!
 
@@ -47,12 +40,10 @@ class GroupsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_group
       @group = Group.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def group_params
       params.require(:group).permit(:hashtag, :name, :details, :capacity, :location, :payment_method)
     end
