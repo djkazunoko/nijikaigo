53cf64c4968d64ad1732f4711a936e7af6168137
djkazunoko <djkazunoko@gmail.com>
2024-03-28 09:38:46 +0900

    feat: カラムに制約を追加してマイグレーションを実行 (#66)
    null: falseを追加、providerカラムとuidカラムの組み合わせに対してユニーク制約を追加
    
    bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_12_051730) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_28_001932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_12_051730) do
     t.string "payment_method", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider", null: false
+    t.string "uid", null: false
+    t.string "name", null: false
+    t.string "image_url", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
 end
