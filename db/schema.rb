fe503932cc096a65b3e942d2d9642112dc60beb2
djkazunoko <djkazunoko@gmail.com>
2024-05-18 12:06:12 +0900

    Groupsテーブルにowner_idカラムを追加
    bin/rails g migration AddOwnerIdToGroups owner_id:bigint → 制約を追加 → bin/rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_03_055814) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_17_094846) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_055814) do
     t.string "payment_method", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "owner_id", null: false
+    t.index ["owner_id"], name: "index_groups_on_owner_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -37,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_055814) do
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
+  add_foreign_key "groups", "users", column: "owner_id"
 end
