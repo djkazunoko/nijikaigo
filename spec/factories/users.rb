f06d17647cc12f75a8da6a25428bad09b67d0f09
djkazunoko <djkazunoko@gmail.com>
2024-05-23 14:19:02 +0900

    groupのfactoriesとuserのfactoriesを関連付け

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :user do
+  factory :user, aliases: [:owner] do
     provider { 'github' }
     uid { '0001' }
     name { 'tester1' }
